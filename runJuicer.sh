7157db0c9d2bf89de296c3c355aca5680ea92263
Eric Davis <ericscottdavis@outlook.com>
2020-01-30 12:34:37 -0500

    Update cluster parameters

--- runJuicer.sh
+++ runJuicer.sh
@@ -22,10 +22,10 @@ python3 -m venv env && source env/bin/activate && pip3 install -r requirements.t
 mkdir -p logs_slurm
 
 ## Execute splitFASTQ snakemake workflow
-snakemake -j 50 -s splitFASTQ --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch --job-name {cluster.name} -p {cluster.partition} -n {cluster.tasks} -N {cluster.nodes} --mem={cluster.mem} -t {cluster.time} --output {cluster.output} --error {cluster.error}"
+snakemake -j 50 -s splitFASTQ --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error}"
 
 ## Execute alignFASTQ snakemake workflow
-snakemake -j 50 -s alignFASTQ --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch --job-name {cluster.name} -p {cluster.partition} -n {cluster.tasks} -N {cluster.nodes} --mem={cluster.mem} -t {cluster.time} --output {cluster.output} --error {cluster.error}"
+snakemake -j 50 -s alignFASTQ --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error}"
 
 ## Success message
 echo "Entire workflow completed successfully!"
