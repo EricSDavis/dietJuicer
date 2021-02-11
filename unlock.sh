69bd41ad95a924bae4e1029ef1a082338bdce24c
Eric Davis <ericscottdavis@outlook.com>
2021-02-11 15:51:34 -0500

    Create unlock.sh for easier unlocking of directory
    unlock.sh makes it easier to restart a workflow that has failed due to slurm errors or been terminated prematurely.

---
+++ unlock.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -login
+
+## Exit if any command fails
+set -e
+
+## Load required modules
+module load python/3.6.6
+
+## Activate virtual environment
+source env/bin/activate
+
+## Execute alignFASTQ snakemake workflow
+snakemake -j 100 --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p -s workflows/alignFASTQ --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
+
+## Deactivate virtual environment
+deactivate
+
+## Success message
+echo -e "\033[0;32mDirectory unlocked, ready to rerun with sbatch dietJuicerCore.sh or sbatch dietJuicerMerge.sh"
