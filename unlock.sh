85ac7b6a51982ae68d9f928306683275e9a49df2
Eric Davis <ericscottdavis@outlook.com>
2021-02-12 10:43:07 -0500

    Include workflow option for unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -9,8 +9,23 @@ module load python/3.6.6
 ## Activate virtual environment
 source env/bin/activate
 
-## Unlock snakemake workflow
-snakemake -j 100 --unlock --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
+case $1 in
+
+    '-h' | '--help' | ' ' | '')
+            echo -e '\e[31mSpecificy which workflow to unlock (i.e. alignFASTQ or buildHIC)'
+            exit 2
+            ;;
+    
+    'alignFASTQ' | 'dietJuicerCore' | 'Core')
+            ## Unlock snakemake workflow
+            snakemake -j 100 --unlock -s workflows/alignFASTQ --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
+            ;;
+
+    'buildHIC' | 'dietJuicerMerge' | 'Merge')
+            ## Unlock snakemake workflow
+            snakemake -j 100 --unlock -s workflows/buildHIC --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
+            ;;
+esac
 
 ## Deactivate virtual environment
 deactivate
