49821ac615e4dde08e6c4852e62008988e9693a3
Eric Davis <ericscottdavis@outlook.com>
2021-02-12 10:11:41 -0500

    Actually add --unlock argument; remove workflow specification
    remove workflow specification

--- unlock.sh
+++ unlock.sh
@@ -9,8 +9,8 @@ module load python/3.6.6
 ## Activate virtual environment
 source env/bin/activate
 
-## Execute alignFASTQ snakemake workflow
-snakemake -j 100 --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p -s workflows/alignFASTQ --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
+## Unlock snakemake workflow
+snakemake -j 100 --unlock --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
 
 ## Deactivate virtual environment
 deactivate
