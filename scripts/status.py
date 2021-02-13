3b21176456519de6198befdfc57f9819318ee4d8
Eric Davis <ericscottdavis@outlook.com>
2021-02-13 11:35:55 -0500

    Fix status issues; move splitFile remove to onsuccess

--- scripts/status.py
+++ scripts/status.py
@@ -4,12 +4,30 @@ import sys
 
 jobid = sys.argv[1]
 
-output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
+status = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
 
-running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED"]
-if "COMPLETED" in output:
-  print("success")
-elif any(r in output for r in running_status):
-  print("running")
-else:
+
+## Adapted from: https://github.com/Snakemake-Profiles/slurm/blob/master/%7B%7Bcookiecutter.profile_name%7D%7D/slurm-status.py
+if "BOOT_FAIL" in status:
   print("failed")
+elif "OUT_OF_MEMORY" in status:
+  print("failed")
+elif "CANCELLED" in status:
+  print("failed")
+elif "COMPLETED" in status:
+  print("success")
+elif "DEADLINE" in status:
+  print("failed")
+elif "FAILED" in status:
+  print("failed")
+elif "NODE_FAIL" in status:
+  print("failed")
+elif "PREEMPTED" in status:
+  print("failed")
+elif "TIMEOUT" in status:
+  print("failed")
+# Unclear whether SUSPENDED should be treated as running or failed
+elif "SUSPENDED" in status:
+  print("failed")
+else:
+  print("running")
